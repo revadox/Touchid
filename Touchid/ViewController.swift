a016c3c6fe09a7f3491cf1d3f2d6e9478e230625
vd <vdbazinga79@gmail.com>
2018-01-18 18:16:06 +0530

    touch id verification

--- Touchid/ViewController.swift
+++ Touchid/ViewController.swift
@@ -8,13 +8,47 @@
 
 import UIKit
 
+import LocalAuthentication
+
 class ViewController: UIViewController {
 
+    @IBAction func btn_touch(_ sender: Any) {
+    
+            let context = LAContext()
+            var error: NSError?
+            
+            if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+                let reason = "Identify yourself!"
+                
+                context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) {
+                    [unowned self] success, authenticationError in
+                    
+                    DispatchQueue.main.async {
+                        if success {
+                            print("sucess")
+                            //self.runSecretCode()
+                        } else {
+                            let ac = UIAlertController(title: "Authentication failed", message: "Sorry!", preferredStyle: .alert)
+                            ac.addAction(UIAlertAction(title: "OK", style: .default))
+                            self.present(ac, animated: true)
+                        }
+                    }
+                }
+            } else {
+                let ac = UIAlertController(title: "Touch ID not available", message: "Your device is not configured for Touch ID.", preferredStyle: .alert)
+                ac.addAction(UIAlertAction(title: "OK", style: .default))
+                present(ac, animated: true)
+            }
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
